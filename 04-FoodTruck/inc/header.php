6b97704ced237f69505d49df4fb3658cd972dc93
karimiha <karim.ihadadene@lepoles.com>
2017-03-09 15:52:00 +0100

    ajout fichier truck

---
+++ 04-FoodTruck/inc/header.php
@@ -0,0 +1,13 @@
+<header>
+  <h1> ALLO CO'TRUCK</h1>
+  <img src="img/logo_africa.jpg" alt="logo">
+  <nav>
+    <ul>
+       <li> <a href="index.php">Accueil</a></li>
+       <li><a href="food_truck.php">page food_truck</a></li>
+       <li><a href="index-Ocouscous.php">page index-couscous</a></li>
+       <li><a href="indexJo.php">page indexJo</a></li>
+       <li><a href="SandWitch.php">page SandWitch</a></li>
+    </ul>
+  </nav>
+</header>
